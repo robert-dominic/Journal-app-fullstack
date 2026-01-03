d935ad2d9521be7557b4c455eccf798cda2fec42
Robert Dominic <devrobert.codes@gmail.com>
2026-01-03 22:26:09 +0000

    chore: adjust App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { EntriesProvider } from "./contexts/EntriesContext";
+import { ModalProvider } from "./contexts/ModalContext";
 import Home from './pages/Home';
 import Journal from './pages/Journal';
 import Editor from "./pages/Editor";
@@ -10,6 +11,7 @@ function App() {
   return (
     <AuthProvider>
       <EntriesProvider>
+        <ModalProvider>
         <Router>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -18,6 +20,7 @@ function App() {
             <Route path="/entry/:id" element={<EntryDetail />} />
           </Routes>
         </Router>
+        </ModalProvider>
       </EntriesProvider>
     </AuthProvider>
   )
